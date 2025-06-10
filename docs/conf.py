29653cac488d41df33184fde4b0611573aef4eb6
Prakriti Gupta <pgupta@caltech.edu>
2025-06-10 15:11:01 -0700

    update theme

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 
 project = 'mktl'
 copyright = '2025, COO'
-author = 'COO'
+author = 'Caltech Optical Observatories (COO)'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -28,5 +28,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "alabaster"
 html_static_path = ['_static']
